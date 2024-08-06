d51876946c906da0fb353fd82ee1076789604985
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-08-06 15:30:45 +0900

    障害物を作りました

--- Assets/Member/Shinbo/Script/Item_Jump.cs
+++ Assets/Member/Shinbo/Script/Item_Jump.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 /// <summary>
 /// ジャンプ力強化：ジャンプ力が1.5～2倍になる
@@ -6,16 +7,27 @@
 public class Item_Jump : ItemController
 {
     GameObject _player;
+    PlayerJump _playerJump;
+    [SerializeField, Tooltip("ジャンプ力の倍率")] float _jumpForceMultiplier;
+    [SerializeField] float _releaseSeconds = 3;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        _playerJump = _player.GetComponent<PlayerJump>();
     }
 
     public override void ItemGet()
     {
-        //PlayerController playerController = _player.GetComponent<PlayerController>();
+        _playerJump.JumpPowerUp(_jumpForceMultiplier);
         Debug.Log("Jump UP");
-        //ジャンプ力を上げるメソッドを呼び出す
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(_releaseSeconds);
+
+        _playerJump.JumpPowerReturn();
     }
 }
---
+++ Assets/Member/Shinbo/Script/ObstacleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 障害物全般のController　継承して使います
+/// 障害物にプレイヤーTagがついたオブジェクトが衝突したら、
+/// </summary>
+public class ObstacleController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //タイムを減らすためのスクリプトを取得する
+            ObstacleCollision();
+        }
+    }
+
+    public virtual void ObstacleCollision()
+    {
+        //障害物に衝突した時の処理を書く
+        Debug.Log("ObstacleCollision");
+    }
+}
