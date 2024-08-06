610ae101d0d77271c8ab16289efd67fa4ff87467
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-08-06 16:31:09 +0900

    障害物を生成して、横方向に飛ばせるようにしました

--- Assets/Member/Shinbo/Script/ObstacleController.cs
+++ Assets/Member/Shinbo/Script/ObstacleController.cs
@@ -4,8 +4,23 @@
 /// 障害物全般のController　継承して使います
 /// 障害物にプレイヤーTagがついたオブジェクトが衝突したら、
 /// </summary>
+
+[RequireComponent(typeof(Rigidbody))]
 public class ObstacleController : MonoBehaviour
 {
+    [SerializeField] float _speed = 10;
+
+    private void Start()
+    {
+        //生成されたとき、横方向にとんでいく
+        Rigidbody rb = GetComponent<Rigidbody>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player.transform.position.x > transform.position.x) rb.velocity = Vector3.right * _speed;
+        else rb.velocity = Vector3.left * _speed;
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
--- Assets/Member/Shinbo/Script/ObstacleGeneration.cs
+++ Assets/Member/Shinbo/Script/ObstacleGeneration.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
 
+/// <summary>
+/// 障害物を生成して、横方向に飛ばす
+/// 烏：２ポイントおき
+/// 飛行機：３ポイントおき
+/// に生成する
+/// </summary>
 public class ObstacleGeneration : MonoBehaviour
 {
     [SerializeField] GameObject _rangeUpper, _rangeLower;
     [SerializeField] GameObject[] _obstaclePrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-        Positioning();
+       // Positioning();
+        ReversePositioning();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void Positioning() //生成
+    public void Positioning() //生成
     {
         float x = Random.Range(_rangeUpper.transform.position.x, _rangeLower.transform.position.x);
+        float y = Random.Range(_rangeUpper.transform.position.y, _rangeLower.transform.position.y);
+        float z = Random.Range(_rangeUpper.transform.position.z, _rangeLower.transform.position.z);
+
+        Instantiate(_obstaclePrefabs[0], new Vector3(x, y, z), Quaternion.identity);
+    }
+
+    public void ReversePositioning() //逆側
+    {
+        float x = Random.Range(_rangeUpper.transform.position.x * -1, _rangeLower.transform.position.x * -1);
         float y = Random.Range(_rangeUpper.transform.position.y, _rangeLower.transform.position.y);
         float z = Random.Range(_rangeUpper.transform.position.z, _rangeLower.transform.position.z);
 
