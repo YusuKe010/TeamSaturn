ad958876f9b4179954c899d58cc3029c4ae8ff14
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-08-06 14:26:52 +0900

    アイテム2種類（無敵・ジャンプ力強化）を作りました

---
+++ Assets/Member/Shinbo/Script/Item_Invincible.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 無敵：障害物に当たっても無効化する
+/// </summary>
+public class Item_Invincible : ItemController
+{
+    GameObject[] _obstacleObj;
+    [SerializeField] float _releaseSeconds = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _obstacleObj = GameObject.FindGameObjectsWithTag("Respawn");
+    }
+
+    public override void ItemGet()
+    {
+        foreach (var obj in _obstacleObj)
+        {
+            BoxCollider col = obj.GetComponent<BoxCollider>();
+            col.enabled = false;
+            StartCoroutine(Release());
+        }
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(_releaseSeconds);
+
+        foreach (var obj in _obstacleObj)
+        {
+            BoxCollider col = obj.GetComponent<BoxCollider>();
+            col.enabled = true;
+        }
+    }
+}
