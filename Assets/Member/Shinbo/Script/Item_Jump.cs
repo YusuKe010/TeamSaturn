29963f1e231b4d1ca6215c9393aeccda6a2bb44b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2024-08-07 15:26:59 +0900

    アイテム

--- Assets/Member/Shinbo/Script/Item_Jump.cs
+++ Assets/Member/Shinbo/Script/Item_Jump.cs
@@ -6,30 +6,50 @@ using UnityEngine;
 /// </summary>
 public class Item_Jump : ItemController
 {
-    GameObject _player;
-    PlayerJump _playerJump;
+    GameObject[] _player;
+    PlayerJump[] _playerJump;
     [SerializeField, Tooltip("ジャンプ力の倍率")] float _jumpForceMultiplier = 1.5f;
     [SerializeField] float _releaseSeconds = 3;
+    Collider _collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        _player = GameObject.FindWithTag("Player");
-        _playerJump = _player.GetComponent<PlayerJump>();
+        _player = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < 2; i++)
+        {
+            _playerJump[i] = _player[i].GetComponent<PlayerJump>();
+        }
     }
 
-    public override void ItemGet()
+    public override void ItemGet(Collider other)
     {
-        _playerJump.JumpPowerUp(_jumpForceMultiplier);
-        AudioPlayer.PlaySE("Jump");
-        Destroy(gameObject, 1);
-        Debug.Log("ジャンプ力上昇中");
-        StartCoroutine(Release());
+        PlayerJump a = other.gameObject.GetComponent<PlayerJump>();
+        for (int i = 0; i < 2; i++)
+        {
+            if (a == _playerJump[i])
+            {
+                _collider = other;
+                _playerJump[i].JumpPowerUp(_jumpForceMultiplier);
+                AudioPlayer.PlaySE("Jump");
+                Destroy(gameObject, 1);
+                Debug.Log("ジャンプ力上昇中");
+                StartCoroutine(Release());
+            }
+        }
     }
 
     IEnumerator Release()
     {
         yield return new WaitForSeconds(_releaseSeconds);
 
-        _playerJump.JumpPowerReturn();
+        for (int i = 0; i < 2; i++)
+        {
+            if (_playerJump[i] == _collider.gameObject.GetComponent<PlayerJump>())
+            {
+                _playerJump[i].JumpPowerReturn();
+            }
+            
+        }
     }
 }
