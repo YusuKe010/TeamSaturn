0c128eaad9f1c8b70946640ddf29fc20274c9d67
Yusuke17 <yuusuke.k1717@gmail.com>
2024-08-07 14:24:32 +0900

    プレイヤーのSE、エフェクトとタイトルで名前の送信を待つ

--- Assets/Member/Kawaguchi/Scripts/InputField.cs
+++ Assets/Member/Kawaguchi/Scripts/InputField.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InputField : MonoBehaviour
+public class InputNameField : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private UnityEngine.UI.InputField _player1Field;
     [SerializeField] private UnityEngine.UI.InputField _player2Field;
     [SerializeField] Ranking _ranking;
-    public void PlayerNameDecision()
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        _ranking.PostData(_player1Field.text, 0, null);
-        _ranking.PostData(_player2Field.text, 0, null);
+        StartCoroutine(PlayerNameDecision(() => SceneLoader.ChangeScene("InGame")));
+    }
+
+    public IEnumerator PlayerNameDecision(Action action)
+    {
+        yield return StartCoroutine(_ranking.PostDataAsync(_player1Field.text, 0, null));
+        yield return StartCoroutine(_ranking.PostDataAsync(_player2Field.text, 0, null));
+        action();
     }
 }
